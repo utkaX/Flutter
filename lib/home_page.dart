141f268b4e7dee3af5af6332f89e186351ded529
utkaX <kaneriyautsav59@gmail.com>
2025-05-16 09:31:11 +0530

    completed

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+
+    int days = 30;
+    String name = "Utsav";
+
+    return Scaffold(
+      appBar: AppBar(
+        title:Center(
+          child: Text("Day 2 of practice"),
+           ),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome $days days flutter practice by ${name[0]}"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
+
+ 
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container
-          (
-            child: Text("Welcome to flutter practice")
-          ),
-        ),
-      ),
+      home: Home(),
     );
   }
 }
