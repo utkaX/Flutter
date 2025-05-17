31edb82373c4623745a23f37561d57a8a331145f
utkaX <kaneriyautsav59@gmail.com>
2025-05-17 10:35:03 +0530

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,72 +5,118 @@ import 'package:flutter_catalog/utils/routes.dart';
 // also we can use fonts from google fonts from pub.dev
 // it has build in library
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   // const MyApp({Key? key}) : super(key: key);
+
+  String username = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: SingleChildScrollView(	
-        child:Column(
-        children: [
-          Image.asset('./assests/images/login_image.png', fit: BoxFit.cover),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('./assests/images/login_image.png', fit: BoxFit.cover),
+            Text(
+              "Welcome $username",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-          ),	
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter username",
-                    labelText: "Username",
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 32.0,
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                    onChanged: (value) {
+                      username = value;
+                      setState(() {});
+                    },
                   ),
-                ),
-                TextFormField(
+                  TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
-                    hintText: "Enter password",
-                    labelText: "Password",                    
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
                   ),
-                ),
-                SizedBox(height: 60),
+                  SizedBox(height: 60),
 
-                ElevatedButton(onPressed: () {
-                  Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  }, 
-                  style: TextButton.styleFrom(
-                    minimumSize: Size(150, 60),
-                    backgroundColor: const Color.fromARGB(255, 76, 93, 219),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  InkWell(
+                    onTap:() async {
+                          setState(() {
+                            changeButton = true;
+                          });
+                          await Future.delayed(Duration(seconds: 1));
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 60 : 150,
+                      height: 60,
+
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(
+                          changeButton ? 20 : 8,
+                        ),
+                      ),
+                      child:
+                          changeButton
+                              ? Icon(Icons.done, color: Colors.white, size: 30)
+                              : Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
                     ),
                   ),
-                  child: Text("Login",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  ),
-                // style: ButtonStyle(
-                //   foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                // ),),
-              ],
+
+                  // ElevatedButton(onPressed: () {
+                  //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(150, 60),
+                  //     backgroundColor: const Color.fromARGB(255, 76, 93, 219),
+                  //     shape: RoundedRectangleBorder(
+                  //       borderRadius: BorderRadius.circular(10),
+                  //     ),
+                  //   ),
+                  //   child: Text("Login",
+                  //     style: TextStyle(
+                  //       fontSize: 22,
+                  //       fontWeight: FontWeight.bold,
+                  //       color: Colors.black,
+                  //     ),
+                  //   ),
+                  //   ),
+                  // style: ButtonStyle(
+                  //   foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  // ),),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
